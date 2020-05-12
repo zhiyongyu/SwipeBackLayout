a73e8f36a569a731b49ca6d983f5f74857a0ffb6
yuzhiyong <1187175968@qq.com>
2020-05-12 17:30:52 +0800

    添加lib

---
+++ lib/src/main/java/com/example/lib/common/SwipeBackActivity.java
@@ -0,0 +1,62 @@
+package com.example.lib.common;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.lib.utils.Utils;
+import com.example.lib.view.SwipeBackLayout;
+
+public class SwipeBackActivity extends AppCompatActivity implements SwipeBackActivityBase{
+
+    private SwipeBackActivityHelper mHelper;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mHelper = new SwipeBackActivityHelper(this);
+        mHelper.onActivityCreate();
+    }
+
+    /**
+     * 此方法。是onCreate完成后的回调。
+     * 表示Activity已经跑起来了
+     */
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mHelper.onPostCreate();
+    }
+
+    /**
+     * 重写findViewById方法，目测估计是留给外部获取SwipeBackLayout用的。
+     * @param id
+     * @return
+     */
+    @Override
+    public <T extends View> T findViewById(int id) {
+        View view = super.findViewById(id);
+        if(view == null && mHelper != null){
+            return (T) mHelper.findViewById(id);
+        }
+        return (T) view;
+    }
+
+    @Override
+    public SwipeBackLayout getSwipeBackLayout() {
+        return mHelper.getSwipeBackLayout();
+    }
+
+    @Override
+    public void setSwipeBackEnable(boolean enable) {
+        getSwipeBackLayout().setEnableGesture(enable);
+    }
+
+    @Override
+    public void scrollToFinishActivity() {
+        Utils.convertActivityToTranslucent(this);
+        getSwipeBackLayout().scrollToFinishActivity();
+    }
+}
---
+++ lib/src/main/java/com/example/lib/common/SwipeBackActivityBase.java
@@ -0,0 +1,18 @@
+package com.example.lib.common;
+
+
+import com.example.lib.view.SwipeBackLayout;
+
+public interface SwipeBackActivityBase {
+    /**
+     * @return the SwipeBackLayout associated with this activity.
+     */
+    public abstract SwipeBackLayout getSwipeBackLayout();
+
+    public abstract void setSwipeBackEnable(boolean enable);
+
+    /**
+     * Scroll out contentView and finish the activity
+     */
+    public abstract void scrollToFinishActivity();
+}
